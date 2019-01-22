5e0e51948d66e078650d36187f898659cf28c511
keyvan ahani <keyvan.ahani@outlook.com>
2019-01-22 14:36:19 +0330

    upload project

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+using System.Linq;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponses(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // there is a validation error
+                return View();
+            }
+        }
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Response.Where(r => r.WillAttend == true));
+        }
+
+    }
+}
---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+
+        [Required(ErrorMessage = "please enter your name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "please enter your email address")]
+        [EmailAddress(ErrorMessage = "please enter a valid email address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string phone { get; set; }
+        [Required(ErrorMessage = "Please Select option")]
+        public bool? WillAttend { get; set; }
+
+    }
+}
